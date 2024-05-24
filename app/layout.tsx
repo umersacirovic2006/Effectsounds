c4e58a891e43b3d3dae99c80a301b47d6e3a1b71
Umer Sacirovic <asacirovic90@gmail.com>
2024-05-24 12:22:31 +0200

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Lato, Syne } from "next/font/google";
 import "./globals.css";
 import "./_assets/globals.scss";
 import Header from "./_components/header";
 import Footer from "./_components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--inter" });
+const lato = Lato({ subsets: ["latin"], variable: "--lato", weight: ['100', '300', '400', '700', '900'] });
+const syne = Syne({ subsets: ["latin"], variable: "--syne" });
 
 export const metadata: Metadata = {
     title: "Effectsounds",
@@ -19,7 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+        <body className={`${inter.variable} ${lato.variable} ${syne.variable}`}>
                 <Header />
                 {children}
                 <Footer />
